f6b771be2476665e3ecd35e0e6e8a192f2e4ee9f
yangjunhyuk333 <chuchoun1234@gmail.com>
2021-03-04 10:02:01 +0900

    [UPDATE]2020-03-04

---
+++ 20210225/firstDay/firstDay/firstDay.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    //워밍업 - 아스키 코드
+    /*char input = ' ';
+    cin >> input;
+    cout << (int)input << endl;*/
+
+    //워밍업 - 숫자의 합
+    /*int n, result = 0;
+
+    cin >> n;
+
+    char arr[1024];
+
+    cin >> arr;
+
+    for (int i = 0; i < n; i++) {
+        result += arr[i] - '0';
+    }
+
+    cout << result;*/
+
+    //워밍업 - 알파벳 찾기
+    char input = ' ';
+
+    cin >> input;
+
+
+
+
+    return 0;
+}
