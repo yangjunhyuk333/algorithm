d2620aa616c791635f0eb58a2bc91f71e8ebb3b5
yangjunhyuk333 <chuchoun1234@gmail.com>
2021-03-08 00:48:59 +0900

    [ADD] first, second

--- 20210305/codingTest/codingTest/first.cpp
+++ 20210305/codingTest/codingTest/first.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
-int main() {
-	char alpha[1000000];
-	char word;
+int plusNum(int a, int b) {
 
-	cin >> alpha;
+	return a + b;
+}
 
-	for (int i = 0; i < strlen(alpha) - 1; i++) {
-		word = alpha[i];
-		for (int j = 0; j < ; j++) {
+int minusNum(int a, int b) {
 
-		}
-	}
+	return a - b;
+}
+
+int mutipleNum(int a, int b) {
+
+	return a * b;
+}
+
+int divideNum(int a, int b) {
+
+	return a / b;
+}
+
+int divideNum2(int a, int b) {
+
+	return a % b;
+}
+
+int main_1() {
+	
+	double a = 0, b = 0;
+
+	cin >> a;
+	cin >> b;
+
+	cout << plusNum(a,b) << endl;
+	cout << minusNum(a, b) << endl;
+	cout << mutipleNum(a, b) << endl;
+	cout << divideNum(a, b) << endl;
+	cout << divideNum2(a, b);
+	
 
 	return 0;
 }
