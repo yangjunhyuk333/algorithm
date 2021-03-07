f164542d47720e32a1f6daabc7dd594b46c4d759
yangjunhyuk333 <chuchoun1234@gmail.com>
2021-03-07 16:14:32 +0900

    [UPDATE]All

---
+++ 20210305/codingTest/codingTest/first.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	char alpha[1000000];
+	char word;
+
+	cin >> alpha;
+
+	for (int i = 0; i < strlen(alpha) - 1; i++) {
+		word = alpha[i];
+		for (int j = 0; j < ; j++) {
+
+		}
+	}
+
+	return 0;
+}
